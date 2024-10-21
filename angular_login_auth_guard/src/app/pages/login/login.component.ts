1820986a71db92d9e760ad90606bdf6575ed8ded
Abhimanyu Kumar <67216514+abhimanyuraj040@users.noreply.github.com>
2024-10-21 06:52:51 +0000

    added login api call logic

--- angular_login_auth_guard/src/app/pages/login/login.component.ts
+++ angular_login_auth_guard/src/app/pages/login/login.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject, NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  providers:[]
 })
 export class LoginComponent {
+  loginObj:any = {
+    "EmailId":"",
+    "Password":""
+  };
+
+  http = inject(HttpClient);
+
+  onLogin(){
+    debugger;
+    console.log("you are inside login function:)");
+    this.http.post("https://freeapi.miniprojectideas.com/api/User/Login", this.loginObj).subscribe((res:any) => {
+      debugger;
+      if(res.result){
+        alert("Welcome!! You have logged in successfully");
+      }
+      else{
+        alert("Incorrent Credentials. Please try again.");
+      }
+    })
+  }
 
 }
